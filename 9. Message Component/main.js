fe4063d3b998ded0a9f23103d745a50821bd42b0
Xavier Seymour <xseymour3@gmail.com>
2018-09-20 05:31:10 -0400

    9. Message Component
    v-show directive
    Bulma Message Modal

---
+++ 9. Message Component/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+	el: '#root',
+	data: {
+		laracast_url: ' https://laracasts.com/series/learn-vue-2-step-by-step/episodes/9?autoplay=true',
+	},
+	methods : {
+
+	},
+	computed : {
+
+	},
+	mounted() {
+
+	},
+});
---
+++ 9. Message Component/message.js
@@ -0,0 +1,27 @@
+var message = Vue.component('bulma-message', {
+	//Custom Attributes of modal
+	props: ['title', 'body'],
+	template: `
+	<article class="message" v-show="isVisible">
+		  <div class="message-header">
+		    {{title}}
+		    <button @click="hideModal" class="delete" aria-label="delete"></button>
+		  </div>
+		  <div class="message-body">
+		    {{body}}
+		  </div>
+		</article>
+	`,
+	//Cannot set data = an object, as a component is not linked to any single instance. Must set data = a function that returns an object
+	data() {
+		return {
+			isVisible: true
+		}
+	},
+	methods: {
+		hideModal() {
+			this.isVisible = false;
+		},
+	},
+});
+
