7f5a092597bf04e1ecab60b76d8b33584453ae50
Xavier Seymour <xseymour3@gmail.com>
2018-10-10 12:59:08 -0400

    15. Single-Use Components and Inline Templates
    Not every component needs to be generic and reusable. Sometimes, a single, view-specific component is exactly what the doctor ordered. In this episode, we'll review the basic concept, and then discuss when you might reach for the inline-template attribute to nest your template directly in your HTML file.

---
+++ 15. Single Use Components/main.js
@@ -0,0 +1,25 @@
+Vue.component('progress-view', {
+
+	data() {
+		return {completionRate: 50};
+	}
+});
+
+var app = new Vue({
+	el: '#root',
+	data: {
+		laracast_url: 'https://laracasts.com/series/learn-vue-2-step-by-step/episodes/15?autoplay=true',
+	},
+	methods : {
+
+	},
+	computed : {
+
+	},
+	mounted() {
+
+	},
+	created() {
+
+	},
+});
