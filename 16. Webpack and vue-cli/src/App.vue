926f6c5d2406a3a4fcba32adf53f77ade4d992d7
Xavier Seymour <xseymour3@gmail.com>
2018-10-10 14:46:34 -0400

    16. Webpack and vue-cli
    Create using: vue init webpack-simple app_name

---
+++ 16. Webpack and vue-cli/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <message title="test" body="Lorem ipsum dolor set amet"></message>
+    <message>Heres another Message!</message>
+  </div>
+</template>
+
+<script>
+  import Message from './components/message.vue'
+  export default {
+    name: 'app',
+    components: {Message},
+    data () {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<style>
+ 
+</style>
---
+++ 16. Webpack and vue-cli/src/components/message.vue
@@ -0,0 +1,35 @@
+<template>
+	<article class="message" v-show="isVisible">
+	  	<div class="message-header">
+	    	{{title_content}}
+		    <button @click="hideModal" class="delete" aria-label="delete"></button>
+	  	</div>	
+	  	<div class="message-body">
+		     <slot>{{body}}</slot>
+		</div>
+	</article>
+</template>
+
+<script>
+	export default {
+		props: ['title', 'body'],
+		data() {
+			return {
+				isVisible: true,
+				title_content: this.title
+			}
+		},
+		methods : {
+			hideModal() {
+				this.isVisible = false;
+			}
+		},
+		created() {
+			this.title_content = (typeof this.title === 'undefined') ? 'Message' : this.title;
+		}
+	}
+</script>
+
+<style>
+
+</style>
