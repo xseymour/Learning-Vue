0769c840909da37d78623bc75ea0f51a7ec133b9
Xavier Seymour <xseymour3@gmail.com>
2018-10-11 10:43:42 -0400

    17. Hot Module Replacement

---
+++ 17. Hot Module Replacement/src/components/counter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h2>The Count is: {{count}}. Making changes wont reset count data!</h2>
+		<p>
+			<button @click="count += 1">Increment</button>
+		</p>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {count: 0};
+		}
+	}
+</script>
+
+<style>
+	
+</style>
