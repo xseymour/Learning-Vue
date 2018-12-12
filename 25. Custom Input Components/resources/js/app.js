38899a8bcd1d125626d2470f183d5cbba99ab65a
Xavier Seymour <xseymour3@gmail.com>
2018-12-12 03:22:30 -0500

    25. Custom Input Components
    You'll surely come across instances in your own projects, where you'd like to wrap a form input within a custom component. This way, you can have a single place to attach all custom sanitization, validation, and behavior. However, in the process, you'll also find that the useful v-model directive no longer works the way it usually does.

--- 25. Custom Input Components/resources/js/app.js
+++ 25. Custom Input Components/resources/js/app.js
@@ -20,7 +20,58 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('coupon', {
+	props: ['value'], //MUST USE VALUE on a cumstom component that uses v-model
+	data() {
+		return {
+			code: '',
+			expired: [
+				'free', 'expired'
+			],
+			valids: [
+				'discount' , 'valid'
+			]
+		}
+	},
+	template: `
+		<div>
+			<label for='coupon_component'>Using v-model on an input within a component:</label>
+			<a href="https://vuejs.org/v2/guide/components-custom-events.html#Customizing-Component-v-model" target="_blank">Vue Docs</a><br/>
+
+			<input id='coupon_component' v-bind:value='value' @input="updateValue($event.target.value)" v-on:keyup.13="checkCoupon()" ref="coupon_input" >
+			<button @click="checkCoupon()">Submit Coupon</button>
+		</div>
+	`,
+	methods: {
+		updateValue(code) {
+			this.code = code;
+			this.$emit('invalid');
+			//Emit
+			this.$emit('input', code);
+		},
+		checkCoupon() {
+			//Do any sanitization, then emit an input event so parent coupon can handle updating data through v-model two way bind
+			//Sanitization / Validation
+			if(this.valids.includes(_.lowerCase(this.code))) {
+				alert('Valid Coupon! Discount will be applied!');
+				// $(this.$refs.coupon_input).addClass('valid_coupon');
+				this.$emit('valid');
+				return;
+			} else {
+				if (_.indexOf(this.expired, _.lowerCase(this.code)) != -1) {
+					alert('This coupon is expired. Sorry!');
+				} else {
+					alert('Invalid Coupon Code');
+				}
+				this.$emit('input', '');
+				this.$refs.coupon_input.focus();
+			}
+		}
+	},
+	mounted() {
+		this.code = this.value;
+	}
+});
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +80,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        laracast_url: 'https://laracasts.com/series/learn-vue-2-step-by-step/episodes/25?autoplay=true',
+        coupon: 'FREEBIE',
+        code_is_valid: false
+    }
 });
--- 25. Custom Input Components/resources/views/welcome.blade.php
+++ 25. Custom Input Components/resources/views/welcome.blade.php
@@ -12,7 +12,17 @@
         <div id="app" class="container">
             <a :href="laracast_url"  target="_blank">Episode 25</a>
             <br><br>
+            <label for="v-model">v-model auto-creates 2 way bind: </label>
+            <br>
+            <input id="v-model" type="text" v-model="coupon">
+            <br>
+            {{--The above v-model binding is syntactic sugar for the following--}}
+            <label for="2_way_bind">manually update control's value based on data and use listener to update data based on control:</label>
+            <br>
+            <input type="text" v-bind:value="coupon" @input="coupon = $event.target.value">
 
+            {{-- Custom vue input component registered to v-model--}}
+            <coupon v-model='coupon' :class="{valid_coupon : code_is_valid}" @valid="code_is_valid = true" @invalid="code_is_valid = false"></coupon>
 
         </div>
 
